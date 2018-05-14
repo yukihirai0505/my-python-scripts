ea90101147976c246bbc6560579c106275a612a1
Yuki Hirai <yukihirai0505@gmail.com>
2018-05-14 11:13:21 +0900

    Add timeline.py

---
+++ twitter/README.md
@@ -0,0 +1,7 @@
+## Get Started
+
+```
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ twitter/timeline.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+import json, config
+from requests_oauthlib import OAuth1Session
+
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+ATS = config.ACCESS_TOKEN_SECRET
+twitter = OAuth1Session(CK, CS, AT, ATS)
+
+url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+params ={'count' : 5}
+req = twitter.get(url, params = params)
+
+if req.status_code == 200:
+    timeline = json.loads(req.text)
+    for tweet in timeline:
+        print(tweet['user']['name']+'::'+tweet['text'])
+        print(tweet['created_at'])
+        print('----------------------------------------------------')
+else:
+    print("ERROR: %d" % req.status_code)
