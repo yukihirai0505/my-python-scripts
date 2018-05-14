71a1bbd317c6f70b27dd9879f641ee7be3f1eeeb
Yuki Hirai <yukihirai0505@gmail.com>
2018-05-14 11:41:18 +0900

    Add get friend ids

---
+++ twitter/friendsIds.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+import json, config
+from requests_oauthlib import OAuth1Session
+
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+ATS = config.ACCESS_TOKEN_SECRET
+twitter = OAuth1Session(CK, CS, AT, ATS)
+
+
+def get_friend_ids():
+    url = "https://api.twitter.com/1.1/friends/ids.json"
+    params = {'screen_name': 'yabaiwebyasan'}
+    req = twitter.get(url, params=params)
+    if req.status_code == 200:
+        return json.loads(req.text)['ids']
+    else:
+        print("ERROR: %d" % req.status_code)
+        return []
+
+
+def lookup_user(ids):
+    url = "https://api.twitter.com/1.1/users/lookup.json"
+    params = {'user_id': ids}
+    lookup_req = twitter.get(url, params=params)
+    if lookup_req.status_code == 200:
+        return json.loads(lookup_req.text)
+    else:
+        print("ERROR: %d" % lookup_req.status_code)
+
+
+friends_ids = get_friend_ids()
+users = lookup_user(friends_ids)
+for user in users:
+    print(user['screen_name'])
